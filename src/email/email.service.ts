c18457fd447813a59ed17e620eae7bf35bc4be72
ivanrenedo <renedoivan@gmail.com>
2026-07-31 23:06:07 +0100

    gmail falla

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -169,8 +169,8 @@ export class EmailService {
       this.transporter = nodemailer.createTransport({
         service: 'gmail',
         host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
+        port: 587,
+        secure: false,
         auth: {
           user,
           pass,
